c457764b0ac83561a960fc7e9b71bddbd6570d68
mingwang <mingwang@academic.rrc.ca>
2024-11-17 12:10:08 +0800

    在ActiveAdmin添加CartItems

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,4 +5,8 @@ class CartItem < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["book_id", "cart_id", "created_at", "id", "updated_at"]
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["book", "cart"]
+  end
 end
